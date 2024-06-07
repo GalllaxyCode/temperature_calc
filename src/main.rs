b3f1a48203c3b79c4a9b2c69e573d093610d289f
Galllaxy <topaccfort@mail.ru>
2024-06-07 21:29:04 +0300

    Logic rework, added kevin full support, organised code

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,99 @@ fn main() {
     let mut input = String::new();
 
     io::stdin()
-    .read_line(&mut input)
-    .expect("Failed to read line!");
+        .read_line(&mut input)
+        .expect("Failed to read line!");
 
     let input = input.trim();
 
     if input.ends_with("C") {
-        let input: String = input.chars().take(input.len() - 1).collect();
-        let input: f64 = input.parse().expect("Failed to convert input into a number!");
-        let formula: f64 = input * 1.8 + 32.0;
+        println!("Print a first letter of temperature you want to convert to.");
+        let mut input2 = String::new();
 
-        println!("{input} Celsius is {formula} Farenheit.");
+        io::stdin()
+            .read_line(&mut input2)
+            .expect("Failed to read line!");
+
+        let input2 = match input2.chars().next() {
+            Some(c) => c,
+            None => {
+                println!("No characters entered! Please enter a character!");
+                return;
+            }
+        };
+
+        if input2 == 'F' {
+            let input: String = input.chars().take(input.len() - 1).collect();
+            let input: f64 = input.parse().expect("Failed to convert input into a number!");
+            let formula: f64 = input * 1.8 + 32.0;
+
+            println!("{input} Celsius is {formula} Farenheit.");
+        } else if input2 == 'K' {
+            let input: String = input.chars().take(input.len() - 1).collect();
+            let input: f64 = input.parse().expect("Failed to convert input into a number!");
+            let formula: f64 = input + 273.15;
+
+            println!("{input} Celsius is {formula} Kelvin.")
+        }
 
     } else if input.ends_with("F") {
-        let input: String = input.chars().take(input.len() - 1).collect();
-        let input: f64 = input.parse().expect("Failed to convert input into a number!");
-        let formula: f64 = (input - 32.0) / 1.8;
-        println!("{input} Farenheit is {formula} Celsius.");
+        println!("Print a first letter of temperature you want to convert to.");
+        let mut input2 = String::new();
+
+        io::stdin()
+            .read_line(&mut input2)
+            .expect("Failed to read line!");
+
+        let input2 = match input2.chars().next() {
+            Some(c) => c,
+            None => {
+                println!("No characters entered! Please enter a character!");
+                return;
+            }
+        };
+
+        if input2 == 'C' {
+            let input: String = input.chars().take(input.len() - 1).collect();
+            let input: f64 = input.parse().expect("Failed to convert input into a number!");
+            let formula: f64 = (input - 32.0) / 1.8;
+            println!("{input} Farenheit is {formula} Celsius.");
+        } else if input2 == 'K' {
+            let input: String = input.chars().take(input.len() - 1).collect();
+            let input: f64 = input.parse().expect("Failed to conver input into a nubmer!");
+            let formula: f64 = (input - 32.0) / 1.8 + 273.15;
+            println!("{input} Farenheit is {formula} Kelvin.")
+        }
+        
     } else if input.ends_with("K") {
-        let input: String = input.chars().take(input.len() - 1).collect();
-        let input: f64 = input.parse().expect("Falied to convert input into a number!");
-        let formula: f64 = input - 273.15;
-        println!("{input} Kelvin is {formula} Celsius");
+        println!("Print a first letter of temperature you want to convert to.");
+        let mut input2 = String::new();
+
+        io::stdin()
+            .read_line(&mut input2)
+            .expect("Failed to read line!");
+
+        let input2 = match input2.chars().next() {
+            Some(c) => c,
+            None => {
+                println!("No characters entered! Please enter a character!");
+                return;
+            }
+        };
+
+        if input2 == 'C' {
+            let input: String = input.chars().take(input.len() - 1).collect();
+            let input: f64 = input.parse().expect("Falied to convert input into a number!");
+            let formula: f64 = input - 273.15;
+            println!("{input} Kelvin is {formula} Celsius.");
+        } else if input2 == 'F' {
+            let input: String = input.chars().take(input.len() - 1).collect();
+            let input: f64 = input.parse().expect("Failed to convert input into a number!");
+            let formula: f64 = (input - 273.15) * 1.8 + 32.0;
+            println!("{input} Kelvin is {formula} Farenheit.")
+        } else {
+            println!("Print a valid character!")
+        }
+        
     } 
     
     else {
