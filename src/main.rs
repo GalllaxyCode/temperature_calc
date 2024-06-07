8a1838915a3a4ac16741717c1d664e00854fe46f
Galllaxy <topaccfort@mail.ru>
2024-06-07 20:50:45 +0300

    release version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io;
+use std::io::{self, Empty};
 
 fn main() {
-    println!("Type the temperature you want to convert! To convert Celsius to Farenheit type with a C on the end and F to convert vice a versa");
+    println!("Type the temperature you want to convert! To convert Celsius to Farenheit type with a C on the end and F to convert vice versa");
 
     let mut input = String::new();
 
@@ -9,11 +9,19 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line!");
 
-    let input: &str = input.trim();
+    let input = input.trim();
 
     if input.ends_with("C") {
-        let input: f64 = input.pop().trim().parse().expect("Write a number!");
+        let input: String = input.chars().take(input.len()-1).collect();
+        let input: f64 = input.parse().expect("Failed to convert input into a number!");
         let formula: f64 = input * 1.8 + 32.0;
-        println!("")
+
+        println!("{input} Celsius is {formula} Farenheit.")
+
+    } else if input.ends_with("F") {
+        let input: String = input.chars().take(input.len()-1).collect();
+        let input: f64 = input.parse().expect("Failed to convert input into a number!");
+        let formula: f64 = (input - 32.0) / 1.8;
+        println!("{input} Farenheit is {formula} Celsius.")
     }
 }
